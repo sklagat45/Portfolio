8724eba8f30dee11ea57fef2fef10b4a832d8993
Samuel Kiplagat <srklagat@gmail.com>
2023-04-17 00:26:28 +0300

    portfolio commit

---
+++ script.js
@@ -0,0 +1,134 @@
+AOS.init();
+
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
+});
+
+// Fixed Nav
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
+
+new TypeIt("#type1", {
+  speed: 140,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("Software Developer", { delay: 600 })
+  .pause(500)
+  .delete(18)
+  .type("UX/UI Designer", { delay: 600 })
+  .pause(500)
+  .delete(14)
+  .type("Web Developer", { delay: 600 })
+  .pause(500)
+  .delete(13)
+  .type("Gamer when he is not coding", { delay: 700 })
+  .pause(500)
+  .delete(28)
+  .go();
+
+new TypeIt("#type2", {
+  speed: 130,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("Learn continually,", { delay: 600 })
+  .pause(1000)
+  .delete(18)
+  .type("There's always one more thing to learn", { delay: 600 })
+  .pause(1500)
+  .delete(38)
+  .type("If at first you don't succeed; call it version 1.0", { delay: 800 })
+  .pause(1500)
+  .delete(50)
+  .go();
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 3, y: -30 });
+gsap.from(".hero a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from(".nav-item", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.2,
+  y: 30,
+  stagger: 0.2,
+});
+gsap.from(".icons span", {
+  opacity: 0,
+  duration: 1,
+  delay: 4,
+  x: -30,
+  stagger: 0.2,
+});
+
+const glide = document.querySelector(".glide");
+if (glide)
+  new Glide(glide, {
+    type: "carousel",
+    startAt: 0,
+    perView: 3,
+    gap: 30,
+    hoverpause: true,
+    autoplay: 2000,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      996: {
+        perView: 2,
+      },
+      768: {
+        perView: 1,
+      },
+    },
+  }).mount();
